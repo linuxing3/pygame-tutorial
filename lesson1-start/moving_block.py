edb2ae7a9f3752be1d5d5199a719858498a68113
linuxing3 <linuxing3@qq.com>
2020-07-14 22:15:06 -0400

    examples

---
+++ lesson1-start/moving_block.py
@@ -0,0 +1,80 @@
+"""
+ Pygame base template for opening a window
+ 
+ Sample Python/Pygame Programs
+ Simpson College Computer Science
+ http://programarcadegames.com/
+ http://simpson.edu/computer-science/
+ 
+ Explanation video: http://youtu.be/vRB_983kUMc
+"""
+ 
+import pygame
+ 
+# Define some colors
+BLACK = (0, 0, 0)
+WHITE = (255, 255, 255)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+ 
+pygame.init()
+ 
+# Set the width and height of the screen [width, height]
+size = (700, 500)
+screen = pygame.display.set_mode(size)
+ 
+pygame.display.set_caption("My Game")
+
+# 矩形的起始位置
+rect_x = 50
+rect_y = 50
+ 
+# 矩形的方向和速度
+rect_change_x = 5
+rect_change_y = 5
+ 
+# Loop until the user clicks the close button.
+done = False
+ 
+# Used to manage how fast the screen updates
+clock = pygame.time.Clock()
+ 
+# -------- Main Program Loop -----------
+while not done:
+    # --- Main event loop
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            done = True
+ 
+    # --- Game logic should go here
+ 
+    # --- Screen-clearing code goes here
+ 
+    # Here, we clear the screen to white. Don't put other drawing commands
+    # above this, or they will be erased with this command.
+ 
+    # If you want a background image, replace this clear with blit'ing the
+    # background image.
+    screen.fill(BLACK)
+ 
+    # --- Drawing code should go here
+    # 画出矩形
+    pygame.draw.rect(screen, WHITE, [rect_x, rect_y, 50, 50])
+    # 移动矩形的起始位置
+    rect_x += rect_change_x
+    rect_y += rect_change_y
+
+    # 如有需要反转移动的方向
+    if rect_y > 450 or rect_y < 0:
+        rect_change_y = rect_change_y * -1
+    if rect_x > 650 or rect_x < 0:
+        rect_change_x = rect_change_x * -1
+
+    # --- Go ahead and update the screen with what we've drawn.
+    pygame.display.flip()
+ 
+    # --- Limit to 60 frames per second
+    clock.tick(60)
+ 
+# Close the window and quit.
+pygame.quit()
